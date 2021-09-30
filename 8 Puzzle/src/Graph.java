9cafded2f642cd62557195bb4c14d05e07f15692
Phineas Ziegler <phinziegler@gmail.com>
2021-09-30 15:01:44 -0400

    added comments and cleaned up code a bit

--- 8 Puzzle/src/Graph.java
+++ 8 Puzzle/src/Graph.java
@@ -4,9 +4,10 @@ import java.util.PriorityQueue;
 
 public class Graph {
 
-    // Node Class
-    private class Node extends Object implements Comparable<Node> {     // graph node
-        // Node Constructor
+    // NODE CLASS
+    private class Node extends Object implements Comparable<Node> {
+
+        // NODE CONSTRUCTOR
         private Node(String state, String move, int gCost, Node parent) {
             this.state = state;
             this.move = move;
@@ -14,15 +15,16 @@ public class Graph {
             this.parent = parent;
         }
 
+        // NODE VARIABLES.
         private int hCost;      // heuristic cost for this node
         private int gCost;      // total cost to get here from initial state.
         private int fCost;      // h + g
-        private String state;   // will also be used as the id.
-        private String move;    // the move taken to get to this state
+        private String state;   // nPuzzle state.
+        private String move;    // the move taken to get to this state --- used to remember the path.
         private Node parent;    // parent of node.
         private LinkedList<Edge> edges = new LinkedList<Edge>();
 
-        // calculate the heuristic using an input heuristic method
+        // CALCULATE HEURISTIC -- also assigns a value to fCost.
         private void calcHeuristic(String h) {
             NPuzzle puzzle = new NPuzzle(8);
             puzzle.setState(this.state);
@@ -46,8 +48,9 @@ public class Graph {
             this.fCost = this.hCost + this.gCost;
         }
 
+        // COMPARE TO --- used by priority queue to sort nodes.
         @Override
-        public int compareTo(Graph.Node o) { // used by priority queue.
+        public int compareTo(Graph.Node o) {
             if (this.fCost < o.fCost) {
                 return -1;
             }
@@ -57,7 +60,7 @@ public class Graph {
             return 0;
         }
 
-        // same state --- compare node states.
+        // same state --- check if a node has the same state as this one.
         public boolean sameState(Node n) {
             n = (Node) n;
             if (this.state.equals(n.state)) {
@@ -67,7 +70,7 @@ public class Graph {
         }
     }
 
-    // Edge Class
+    // EDGE CLASS
     private class Edge { // holds a cost, endNode, and startNode
         private Node endNode;
 
@@ -76,7 +79,7 @@ public class Graph {
         }
     }
 
-    // Graph Constructor
+    // CONSTRUCTOR
     public Graph(NPuzzle puzzle, int maxNodes, String heuristic, int k) {
         this.rootNode = new Node(puzzle.getState(), "", 0, null);
         this.rootNode.gCost = 0;
@@ -86,6 +89,7 @@ public class Graph {
         this.k = k;
     }
 
+    // PRIVATE VARIABLES FOR GRAPH
     private Node rootNode;
     private int maxNodes;
     private String heuristic;
@@ -93,17 +97,17 @@ public class Graph {
     private int moveCost = 2;
     private String goalState = "b12 345 678 ";
 
-    // maxNodes --- set max nodes to travel
+    // SET MAX NODES
     public void maxNodes(int n) {
         this.maxNodes = n;
     }
 
-    // addEdge
+    // ADD EDGE
     private void addEdge(Node n1, Node n2) {
         n1.edges.add(new Edge(n2));
     }
 
-    // for commandReader to call.
+    // CALLED BY COMMAND READER
     public void solve(String alg) {
         switch (alg) {
             case "A-star":
@@ -123,7 +127,7 @@ public class Graph {
     /////////////////////////
     
 
-    // A-Star
+    // A-STAR SEARCH
     private void aStar() {
         PriorityQueue<Node> frontier = new PriorityQueue<Node>();   // states to be explored.
         LinkedList<Node> explored = new LinkedList<Node>();         // already explored states.
@@ -132,8 +136,9 @@ public class Graph {
         int nodesExplored = 0;
         System.out.println("A* search from state " + this.rootNode.state + "with heuristic " + "\'" + this.heuristic + "\'.");
         while (!frontier.isEmpty()) {
-
             Node curr = frontier.poll();
+
+            // CHECK IF GOAL STATE HAS BEEN FOUND.
             if (curr.state.equals(this.goalState)) {
                 System.out.println("Reached Goal after " + nodesExplored + " nodes explored.");
                 this.printPath(curr);
@@ -142,6 +147,7 @@ public class Graph {
 
             explored.add(curr);
 
+            // CHECK IF MAX NODES REACHED
             nodesExplored++;
             if (this.maxNodes > 0 && this.maxNodes < nodesExplored) {
                 System.out.println("Error: Max number of nodes searched (" + this.maxNodes + ").");
@@ -174,19 +180,19 @@ public class Graph {
                     frontier.remove(same); 
                 }
 
-                // n was not in frontier, and was not previously explored. add it to frontier.
+                // ADD N TO FRONTIER
                 if (!frontier.contains(same) && !explored.contains(eSame)) {
                     frontier.add(n);
                 }
             }
         }
-        // failed
+        // ALGORITHM FAILED TO FIND A SOLUTION
         System.out.println("Failed to find solution.");
         return;
 
     }
 
-    // expand a node -- Helper for A*
+    // EXPAND A NODE FROM ITS CURRENT STATE --- helper for searches. Also assigns a gCost.
     private void expand(Node curr) {
         curr.edges.clear();
         String[] moveList = new String[] { "right", "down", "left", "up" };
@@ -204,8 +210,7 @@ public class Graph {
         }
     }
 
-    // // looks through the frontier and returns a node with a state matching that of
-    // // n. --- helper for A*
+    // RETURNS A NODE THAT HAS THE SAME STATE AS THE INPUT NODE --- used to find out if a node equivalent to n is in the list.
     private Node getNodeWithSameState(LinkedList<Node> testNodes, Node n) {
         Iterator<Node> iterator = testNodes.iterator();
         while (iterator.hasNext()) {
@@ -216,6 +221,7 @@ public class Graph {
         }
         return new Node("", "", 0, null); // impossible to exist in frontier.
     }
+    // SAME AS ABOVE -- just takes a priority queue as input.
     private Node getNodeWithSameState(PriorityQueue<Node> frontier, Node n) {
         Iterator<Node> iterator = frontier.iterator();
         while (iterator.hasNext()) {
@@ -227,7 +233,7 @@ public class Graph {
         return new Node("", "", 0, null); // impossible to exist in frontier.
     }
 
-    // printpath --- helper for A*
+    // PRINT PATH OF A NODE --- helper for searches.
     private void printPath(Node end) {
         System.out.println("Solution Path:");
         Node curr = end;
@@ -246,7 +252,7 @@ public class Graph {
         System.out.print("(" + moveList.size() + " moves)\n");
     }
 
-    // helper for A*
+    // HELPER FOR A*
     private Node getEqualNode(LinkedList<Node> explored, Node n) {
         Iterator<Node> iterator = explored.iterator();
         while (iterator.hasNext()) {
@@ -258,64 +264,21 @@ public class Graph {
         return new Node("", "", 0, null); // impossible to exist in frontier.
     }
 
-    // Beam Search
-
-    // beam search is A* with a fixed frontier.
-    // only add to the frontier if it is one of the best candidates
-
-    /* PSEUDOCODE
-
-        bestNodes.add(rootNode)  // best is a list of length k
-        frontier = new list;
-
-        while(!frontier.isEmpty())
-            foreach node in frontier
-                explored.add(node)
-
-                if(node.state == goalState)
-                    printpath()
-                    return
-
-                expand(node)
-                foreach n in node
-                    if !frontier.contains(n)
-                        //frontier.add(n)
-                        testNodes.add(n)    // a list of length k^b
-                .
-            .
-            allNodes = bestNodes.concat(frontier)
-            allNodes.sort()
-            bestNodes = allNodes.subList(0, k)  // new list of best k nodes
-        .
-
-    */
-
-    // TODO--- Implement an explored category. All nodes rejected by testNodes are bye bye.
-
+    // LOCAL BEAM SEARCH
     public void beam(int k) {
         System.out.println("Beam search with k = " + k + " from state " + this.rootNode.state + ": ");
         LinkedList<Node> bestNodes = new LinkedList<Node>();
         LinkedList<Node> explored = new LinkedList<Node>();
-        LinkedList<Node> prevBest = new LinkedList<Node>();
         bestNodes.add(this.rootNode);
         int nodesSearched = 0;
 
         while(true) {
             PriorityQueue<Node> allNodes = new PriorityQueue<Node>();
             LinkedList<Node> testNodes = new LinkedList<Node>();
-            testNodes.clear();
-
-
-            // // DETECT WHEN STUCK
-            // if(this.isSameList(bestNodes, prevBest)) {
-            //     System.out.println("Error: All nodes stuck at local min.");
-            //     return;
-            // }
-            // prevBest.clear();
-            // prevBest = bestNodes;
-            
 
             Iterator<Node> besterator = bestNodes.iterator();
+
+            // FOR ALL BEST K STATES
             while(besterator.hasNext()) {
                 Node curr = besterator.next();
                 nodesSearched++;
@@ -333,17 +296,11 @@ public class Graph {
                     return;
                 }
 
-                // EXPAND CURRENT NODE
+                // EXPAND CURRENT NODE -- ADD CURRENT NODE TO EXPLORED
                 this.expand(curr);
                 explored.add(curr);
 
-                // ADD CURR TO TESTNODES (avoiding duplicates)
-                // Node same1 = getNodeWithSameState(testNodes, curr);
-                // if(!testNodes.contains(same1)) {
-                //     testNodes.add(curr);
-                // }
-
-                // FOR EACH N in CURRENT NODE, ADD TO TESTNODES AND EVALUATE (avoiding duplicates)
+                // FOR EACH N in CURRENT NODE, ADD TO TESTNODES AND EVALUATE (avoiding duplicates and already explored states)
                 Iterator<Edge> edgeit = curr.edges.iterator();
                 while(edgeit.hasNext()) {
                     Node n = edgeit.next().endNode;
@@ -357,12 +314,17 @@ public class Graph {
                 }
             }
 
-            allNodes.clear();
-            allNodes.addAll(testNodes);
+            // STUCK AT LOCAL MIN --- FAIL CONDITION
+            if(testNodes.size() == 0) {
+                System.out.println("Error: all beams stuck at local min.");
+                return;
+            }
 
+            allNodes.addAll(testNodes);     // ALLNODES IS A PRIORITY QUEUE. IT SORTS THE TEST NODES BY VALUE.
+            
+            // ADD K NODES FROM ALLNODES TO BEST --- BECAUSE ALLNODES IS SORTED, THIS PUTS THE K LOWEST VALUE NODES IN BEST.
+            int i = 0;
             bestNodes.clear();
-
-            int i=0;
             while(!allNodes.isEmpty() && i < k) {
                 bestNodes.add(allNodes.poll());
                 i++;
@@ -370,43 +332,10 @@ public class Graph {
         }
     }
 
-    private void printList(LinkedList<Node> list) {
-        Iterator<Node> it = list.iterator();
-        while(it.hasNext()) {
-            Node curr = it.next();
-            System.out.print(curr.state + "F(n) = " + curr.fCost + " | ");
-        }
-        System.out.print("\n");
-    }
-
-    // the evaluation function for beam
+    // EVALUATION FUNCTION FOR BEAM
     private void evaluate(Node n) {
         n.calcHeuristic("h2");          // use manhattan distance.
         n.fCost = n.fCost - n.gCost;    // we ignore the gCost assigned to the node from when it was expanded
-    }
-
-    // checks if two lists are the same.
-    private boolean isSameList(LinkedList<Node> bestNodes, LinkedList<Node> prevBest) {
-        //System.out.println();
-        //this.printList(bestNodes);
-
-        if(prevBest.size() <= k) {
-            return false;
-        }
-        
-
-        Iterator<Node> besterator = bestNodes.iterator();
-        while(besterator.hasNext()) {       // if there is a state present in bestNodes that does not exist in prevBest, return false
-            Node best = besterator.next();
-            Node same = this.getNodeWithSameState(prevBest, best);  // same is a node that is equal to best, but present in prevBest (if it exists)
-
-            if(!prevBest.contains(same)) {       // best is NOT in prevBest
-                return false;       // not the same list
-            }
-
-            //System.out.println(best.state + " is in prev, " + same.state);
-        }
-        return true;
     }
     
 }
