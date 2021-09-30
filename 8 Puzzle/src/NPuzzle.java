4309a3067a15cecdbec1ce5caf4f2875f4b5d2ae
Phineas Ziegler <phinziegler@gmail.com>
2021-09-30 15:21:12 -0400

    code cleanup and commenting

--- 8 Puzzle/src/NPuzzle.java
+++ 8 Puzzle/src/NPuzzle.java
@@ -1,7 +1,7 @@
 
 public class NPuzzle {
 
-	// points at blank 
+	// POINTER CLASS --- Npuzzle holds the location of its blank spot in a pointer.
 	private class Pointer {
 		private Pointer(int r, int c) {
 			this.setCoords(r, c);
@@ -18,19 +18,19 @@ public class NPuzzle {
 		}
 	}
 
-	// Constructor
+	// NPUZZLE CONSTRUCTOR --- Initially designed to handle n-puzzles, but it was too difficult. Input should always be 8 (for now)
 	public NPuzzle(int n) {
 		int square = n + 1;
 		this.width = (int)Math.sqrt(square);
 		this.state = new Integer[this.width][this.width];
 	}
 
-	// Private variables
+	// PRIVATE VARIABLES
 	private int width;
 	private Integer[][] state;
 	private Pointer bPoint;	// [r][c] of the blank slot --- tracking this lets us do moves faster
 
-	// set state
+	// SET STATE --- set state to input state
 	public void setState(String state) {	// "b12 345 678"
 		try {
 			int spaces = 0;
@@ -59,6 +59,7 @@ public class NPuzzle {
 		}
 	}
 
+	// GET STATE --- convert the Integer[][] into a string representing its state.
 	public String getState() {
 		StringBuilder sb = new StringBuilder();
 
@@ -76,12 +77,12 @@ public class NPuzzle {
 		return sb.toString();
 	}
 
-	// String Representation of State
+	// PRINT STATE --- not really used.
 	public void printState() {
 		System.out.println(getState());
 	}
 
-	// Visual Representation of State
+	// PRINT INT STATE --- outputs to the console the 'matrix' of the puzzle.
 	public void printIntState() {
 		for(int r = 0; r < this.width; r++) {
 			System.out.print("| ");
@@ -98,7 +99,7 @@ public class NPuzzle {
 		}
 	}
 
-	// move
+	// MOVE --- perform a move in input direction
 	public boolean move(String direction) {
 
 		int row = this.bPoint.row;
@@ -129,7 +130,7 @@ public class NPuzzle {
 		return this.swap(this.bPoint, newPos);
 	}
 
-	// swap
+	// SWAP --- helper method for move --- swaps a pointer with a number.
 	private boolean swap(Pointer blank, Pointer num) {
 		if(num.row >= 0 && num.row < this.width && num.col >= 0 && num.col < this.width) {
 			this.state[this.bPoint.row][this.bPoint.col] = this.state[num.row][num.col];
@@ -142,7 +143,7 @@ public class NPuzzle {
 		return false;
 	}
 	
-	// randomizeState
+	// RANDOMIZE STATE --- performs n random moves. Does not count a move if it was an invalid move.
 	public void randomizeState(int n) {
 		
 		String[] moves = new String[]{"up","down","left","right"};
@@ -150,24 +151,25 @@ public class NPuzzle {
 		for(int i = 0; i < n; i++) {
 			String randMove = moves[(int)(Math.random() * moves.length)];
 			
-			if(this.move(randMove)) {
-				//System.out.println(randMove);
-			}
-			else {
+			if(!this.move(randMove)) {
 				i--;	// tried a move that couldnt be done, try a new move.
 			}
 		}
 	}
 
+
 	//////////////////////////////
 	/// HEURISTICS CALCULATION /// 
 	//////////////////////////////
 
+	
+	// H0 --- NO HEURISTIC --- returns 0 always.
 	public int heuristic0() {
 		return 0;
 	}
 
-	public int heuristic1() {	// number of misplaced tiles.
+	// H1 --- NUM MISPLACED TILES --- number of misplaced tiles.
+	public int heuristic1() {
 		int sum = 0;
 		for(int r = 0; r < this.state.length; r++) {
 			for(int c = 0; c < state[r].length; c++) {
@@ -181,20 +183,20 @@ public class NPuzzle {
 				}
 			}
 		}
-		//System.out.println("H1 = " + sum);
 		return sum;
 	}
 
-	public int heuristic2() {	// manhattan distance (sum of tile distance to their correct position)
+	// H2 --- MANHATTAN DISTANCE --- sum of tile distance to their correct position
+	public int heuristic2() {
 		int sum = 0;
 		for(int r = 0; r < this.state.length; r++) {
 			for(int c = 0; c < this.state[r].length; c++) {
 				sum += this.h2Dist(r,c);
 			}
 		}
-		//System.out.println("H2 = " + sum);
 		return sum;
 	}
+	// helper for H2
 	private int h2Dist(int r, int c) {
 		int val;
 		if(this.state[r][c] == null) {
@@ -208,6 +210,8 @@ public class NPuzzle {
 		int dist = Math.abs(r - goodRow) + Math.abs(c - goodCol);
 		return dist;
 	}
+
+	// H3 --- MANHATTAN DISTANCE / 2 --- obsolete when move cost is considered to be > 1
 	public int heuristic3() {
 		return (this.heuristic2()/2);
 	}
